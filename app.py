8a83fef7d5d74089880ea1f5dddad0ba67dd5991
19ecal <96256923+mrlegohead0x45@users.noreply.github.com>
2021-12-30 13:41:08 +0000

    Complete app.py

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 import flask
 import base64
+import pickle
+import traceback
 
 app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
-	data = flask.request.args.get("data") 
+	data = flask.request.args.get("data")
 
 	if data is None:
-		with open("image.jpg.b64", "r") as file:	
-			return flask.render_template("index.html", image=file.read())
+		with open("image.jpg.b64", "r") as file:
+			image = file.read()
+		
+		return flask.render_template("index.html", image=image)
+
+	decoded = base64.urlsafe_b64decode(data)
+	unpickled = pickle.loads(decoded)
+
+	return flask.render_template("index.html", image=unpickled)
 
 	
 app.run("0.0.0.0", 8000)
